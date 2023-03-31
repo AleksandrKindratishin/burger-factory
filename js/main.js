b2c122942e22c2c302ecf7c82cf5c8ebd7e95e40
Kindra24 <alexandr24kindra@gmail.com>
2023-03-31 22:31:57 +0300

    added all blocks for dsctop version end pop-up forms

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+  }
+  
+  function closePopup() {
+    document.getElementById("popup").style.display = "none";
+  }
+  
+
+  var links = document.querySelectorAll('.smooth-scroll');
+    
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', function (event) {
+            event.preventDefault();
+
+            var targetElement = document.querySelector(this.hash);
+            var scrollDistance = targetElement.offsetTop;
+
+            var duration = 300; // длительность анимации в миллисекундах
+            var startingPosition = window.pageYOffset; // начальная позиция прокрутки
+
+            var startTime = null;
+
+            function scrollToTarget(timestamp) {
+                if (!startTime) startTime = timestamp;
+
+                var progress = timestamp - startTime;
+                var percentage = Math.min(progress / duration, 1);
+
+                var currentPosition = startingPosition + (scrollDistance * percentage);
+
+                window.scrollTo(0, currentPosition);
+
+                if (progress < duration) {
+                    window.requestAnimationFrame(scrollToTarget);
+                }
+            }
+
+            window.requestAnimationFrame(scrollToTarget);
+        });
+    }
